a58b8c23c44189230312aa79ada075b41984df88
asanlu <1925751694@qq.com>
2019-09-10 14:23:44 +0800

    add axios

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,30 +1,54 @@
-import fetch from 'dva/fetch';
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+import qs from 'qs';
+import WEBSITE_CONFIG from '../../config';
 
-function parseJSON(response) {
-  return response.json();
-}
+interface RequestOption extends AxiosRequestConfig {
+  method?: | 'get' | 'post';
+  url: string;
+  dataType?: 'form';    // 标志form表单传输方式
+};
 
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
+// 添加请求拦截器
+axios.interceptors.request.use((config: AxiosRequestConfig) => {
+  console.log('request....', config)
+
+  // post form表单提交形式
+  if (config.headers.dataType === 'form') {
+    config.headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    config.data = qs.stringify(config.data);
   }
 
-  const error = new Error(response.statusText);
-  error.response = response;
-  throw error;
-}
+  // 开发环境添加 '/api' 前缀做跨域
+  config.url = WEBSITE_CONFIG.apiPrefix + config.url!;
 
-/**
- * Requests a URL, returning a promise.
- *
- * @param  {string} url       The URL we want to request
- * @param  {object} [options] The options we want to pass to "fetch"
- * @return {object}           An object containing either "data" or "err"
- */
-export default function request(url: string, options?: any) {
-  return fetch(url, options)
-    .then(checkStatus)
-    .then(parseJSON)
-    .then(data => ({ data }))
-    .catch(err => ({ err }));
-}
+  return config;
+}, error => {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use((response: AxiosResponse) => {
+  // 对响应数据做点什么
+  return response;
+}, (error: AxiosError) => {
+  return Promise.reject(error);
+});
+
+const request = ({ method, url, data, params, dataType }: RequestOption) => {
+  const requestConfig: AxiosRequestConfig = {
+    method,
+    url,
+    data,
+    params,
+    headers: {
+      dataType: dataType ? dataType : 'json'
+    },
+    timeout: 5 * 1000,
+  };
+  return axios(requestConfig);
+};
+
+export default request;
